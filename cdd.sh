481bc0608c530e5214925ff9fb1596d9f8197bb4
Ji-Hun Kim <jihuun.k@gmail.com>
2021-08-13 21:27:57 +0900

    cdd: refactoring and implement path argument
    Passing a specific path argument is also possible (e.g. cdd ~/Downloads)
    
    Signed-off-by: Ji-Hun Kim <jihuun.k@gmail.com>

--- cdd.sh
+++ cdd.sh
@@ -1,28 +1,42 @@
+#! /bin/bash
 # Cdd is bash shell command which can help to go to previous directory easily
 # Author : Ji-Hun Kim (jihuun.k@gmail.com)
-# Version : v0.0.1
+# Version : v0.1.0
 
-#! /bin/bash
+function print_usage() {
+        echo "cdd: easy to go to previous directory"
+        echo "	Usage: cdd [number]"
+        echo "	Example: cdd 3 == cd ../../../"
+}
+
+function get_cnt() {
+        [[ -z ${1} ]] && echo 0 || echo $((${1} - 1))
+}
+
+function cd_dir() {
+        [[ -e "${1}" && -d "${1}" ]] && cd ${1}
+}
 
 function cd_back() {
-	DIR="./"
-	OPTION=$1
-
-	if [[ $@ -eq 0 ]]; then
-		cd ../
-	elif [[ "$@" == "--help" ]]; then
-		echo "[cdd] : easy to go to previous directory"
-		echo "Usage : cdd \"number\""
-		echo "Example : cdd 3 == cd ../../../"
-	else
-		CNT=$OPTION
-		while [ $CNT -gt 0 ]
-		do
-			DIR+="../"
-			CNT=$(($CNT - 1))
-		done
-		echo go to $DIR
-		cd $DIR
-	fi
+        local DIR="./"
+        local CNT=$(get_cnt ${1})
+        while true
+        do
+                DIR+="../"
+                [[ $CNT -gt 0 ]] || break
+                CNT=$(($CNT - 1))
+        done
+        cd $DIR
 }
-alias cdd='cd_back'
+
+function cdd_func() {
+        local ARG=${1}
+        if [[ ${ARG} =~ ^[0-9]+$ ]] || [[ -z ${ARG} ]]; then
+                cd_back ${ARG}
+        elif [[ ${ARG} == "-h" || ${ARG} == "--help" ]]; then
+                print_usage
+        else
+                cd_dir ${ARG}
+        fi
+}
+alias cdd='cdd_func'
