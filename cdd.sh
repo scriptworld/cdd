7314f67f8024b8e315189d5776f2104883f83b62
Ji-Hun Kim <jihuun.k@gmail.com>
2021-08-16 15:36:27 +0900

    cdd: update version 0.2.1
    - implement argument auto completion for bash
    - fix path name
    
    Signed-off-by: Ji-Hun Kim <jihuun.k@gmail.com>

--- cdd.sh
+++ cdd.sh
@@ -1,7 +1,7 @@
 #! /bin/bash
 # Cdd is bash shell command which can help to go to previous directory easily
 # Author : Ji-Hun Kim (jihuun.k@gmail.com)
-# Version : v0.1.1
+# Version : v0.2.1
 
 source cdd_func.sh
 
