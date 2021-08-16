fed99d3e946ec2c273b8fd669200f399b3124fe7
Ji-Hun Kim <jihuun.k@gmail.com>
2021-08-16 14:49:00 +0900

    cdd: seperate functions and rename
    Signed-off-by: Ji-Hun Kim <jihuun.k@gmail.com>

--- cdd.sh
+++ cdd.sh
@@ -1,42 +1,18 @@
 #! /bin/bash
 # Cdd is bash shell command which can help to go to previous directory easily
 # Author : Ji-Hun Kim (jihuun.k@gmail.com)
-# Version : v0.1.0
+# Version : v0.1.1
 
-function print_usage() {
-        echo "cdd: easy to go to previous directory"
-        echo "	Usage: cdd [number]"
-        echo "	Example: cdd 3 == cd ../../../"
-}
+source cdd_func.sh
 
-function get_cnt() {
-        [[ -z ${1} ]] && echo 0 || echo $((${1} - 1))
-}
-
-function cd_dir() {
-        [[ -e "${1}" && -d "${1}" ]] && cd ${1}
-}
-
-function cd_back() {
-        local DIR="./"
-        local CNT=$(get_cnt ${1})
-        while true
-        do
-                DIR+="../"
-                [[ $CNT -gt 0 ]] || break
-                CNT=$(($CNT - 1))
-        done
-        cd $DIR
-}
-
-function cdd_func() {
+function __cdd_main() {
         local ARG=${1}
         if [[ ${ARG} =~ ^[0-9]+$ ]] || [[ -z ${ARG} ]]; then
-                cd_back ${ARG}
+                __cdd_cd_back ${ARG}
         elif [[ ${ARG} == "-h" || ${ARG} == "--help" ]]; then
-                print_usage
+                __cdd_usage
         else
-                cd_dir ${ARG}
+                __cdd_cd_path ${ARG}
         fi
 }
-alias cdd='cdd_func'
+alias cdd=' __cdd_main'
---
+++ cdd_func.sh
@@ -0,0 +1,31 @@
+#! /bin/bash
+
+function __cdd_usage() {
+        echo "cdd: easy to go to previous directory"
+        echo "	Usage: cdd [number]"
+        echo "	Example: cdd 3 == cd ../../../"
+}
+
+function __cdd_nr_prev() {
+        [[ -z ${1} ]] && echo 0 || echo $((${1} - 1))
+}
+
+function __cdd_cd_path() {
+        [[ -e "${1}" && -d "${1}" ]] && cd ${1}
+}
+
+function __cdd_prev_path() {
+        local DIR="./"
+        local CNT=$(__cdd_nr_prev ${1})
+        while true
+        do
+                DIR+="../"
+                [[ $CNT -gt 0 ]] || break
+                CNT=$(($CNT - 1))
+        done
+        echo $DIR
+}
+
+function __cdd_cd_back() {
+	cd $(__cdd_prev_path ${1})
+}
